72679e9e22da7c5c4e9478cd227b7d289452d66a
Abhishek <tripathiabhishek868@gmail.com>
2022-02-09 00:35:02 +0530

    Integration with Swagger

--- app.py
+++ app.py
@@ -2,14 +2,29 @@ from flask import Flask
 from apispec_webframeworks.flask import FlaskPlugin
 from market_tracker.settings import SECRET_KEY
 from market_tracker.views.market_api import market_apis as market_api_blueprint
+from flask_cors import CORS, cross_origin
+from flask_swagger_ui import get_swaggerui_blueprint
 
 def create_app():
     app=Flask(__name__)
+    cors = CORS(app)
+
+    SWAGGER_URL = '/swagger'
+    API_URL = '/static/swagger.json'
+    SWAGGERUI_BLUEPRINT = get_swaggerui_blueprint(
+        SWAGGER_URL,API_URL,
+        config={
+            'app_name': "Python-Flask-REST"
+        }
+    )
+    app.register_blueprint(SWAGGERUI_BLUEPRINT, url_prefix=SWAGGER_URL)
+    ### end swagger specific ###
 
     from .index import main as main_blueprint
     app.register_blueprint(main_blueprint,url_prefix='/api/v1')
     app.register_blueprint(market_api_blueprint,url_prefix='/api/v1')
     app.config['SECRET_KEY']=SECRET_KEY
+    app.config['CORS_HEADERS'] = 'Content-Type'
     return app
 
     
